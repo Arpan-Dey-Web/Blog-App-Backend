26e621428d951976b5f4699c084bad145edba678
Arpan Dey <arpandey.web@gmail.com>
2026-01-08 07:37:31 +0600

    emai verification added

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,14 @@ import express, { Application } from "express";
 import { PostRouter } from "./modules/post/post.routes";
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth";
+import cors from "cors";
 const app: Application = express()
 
+
+app.use(cors({
+    origin: process.env.APP_URL || "http://localhost:4000",
+    credentials: true
+}))
 app.all('/api/auth/*splat', toNodeHandler(auth)); //Note -> *splat dite hobe
 
 app.use(express.json())
